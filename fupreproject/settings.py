e52d60e3086433cd929ba94da6d93770856778da
wxxnston <112559764+wxxnston@users.noreply.github.com>
2022-12-08 20:26:43 +0100

    Update settings.py

--- fupreproject/settings.py
+++ fupreproject/settings.py
@@ -122,9 +122,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/img/'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'assets'),
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -132,6 +130,6 @@ STATICFILES_DIRS=[
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'), )
 
 django_heroku.settings(locals())
